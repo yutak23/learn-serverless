ef3fc39ab416586f0b7b80552776147321dc54b7
Yuta Katayama <yutapilafu@gmail.com>
2022-04-24 17:25:36 +0900

    eslintを導入

--- handler.js
+++ handler.js
@@ -1,15 +1,11 @@
-'use strict';
-
-module.exports.hello = async (event) => {
-	return {
-		statusCode: 200,
-		body: JSON.stringify(
-			{
-				message: 'Go Serverless v3.0! Your function executed successfully!',
-				input: event
-			},
-			null,
-			2
-		)
-	};
-};
+module.exports.hello = async (event) => ({
+	statusCode: 200,
+	body: JSON.stringify(
+		{
+			message: 'Go Serverless v3.0! Your function executed successfully!',
+			input: event
+		},
+		null,
+		2
+	)
+});
