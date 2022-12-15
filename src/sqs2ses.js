e611400d259746ed74c26a9d7c339476165f6168
Yuta Katayama <yutapilafu@gmail.com>
2022-12-15 18:08:36 +0900

    feat: DynamodbでSQSからのメッセージ重複イベントを排除する

--- src/sqs2ses.js
+++ src/sqs2ses.js
@@ -1,6 +1,9 @@
 import { strict as assert } from 'assert';
 import { createLogger, format, transports } from 'winston';
 import { SESv2Client, SendEmailCommand } from '@aws-sdk/client-sesv2';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+import { DateTime } from 'luxon';
 
 const logger = createLogger({
 	level: 'info',
@@ -73,6 +76,12 @@ export const handler = async (event) => {
 
 	const isLocal = stage === 'local';
 
+	const dynamoDBClient = new DynamoDBClient(
+		isLocal ? { endpoint: 'http://localhost:4566', region } : { region }
+	);
+	const ddbDocClient = DynamoDBDocument.from(dynamoDBClient);
+	const tableName = `sqs2ses-queue-table-${stage}`;
+
 	const sesClient = new SESv2Client(
 		isLocal ? { endpoint: 'http://localhost:8005', region } : { region }
 	);
@@ -81,7 +90,34 @@ export const handler = async (event) => {
 		await Promise.all(
 			event.Records.map(async (message) => {
 				try {
+					const { body: jsonStringBody } = message;
+					const body = JSON.parse(jsonStringBody);
+					assert.ok(body.createdAt, `message.body must contain createdAt`);
+
+					const result = await ddbDocClient.get({
+						TableName: tableName,
+						Key: { md5: message.md5OfBody }
+					});
+					if (result.Item) {
+						logger.info({
+							message: `already send mail`,
+							md5OfBody: message.md5OfBody
+						});
+						return;
+					}
+
 					await sendmail({ sesClient, message, sesIdentityArn });
+
+					const ttl = DateTime.now().plus({ days: 1 }).toUnixInteger();
+					await ddbDocClient.put({
+						TableName: tableName,
+						Item: { md5: message.md5OfBody, ttl }
+					});
+					logger.info({
+						message: `put data`,
+						md5OfBody: message.md5OfBody,
+						ttl
+					});
 				} catch (e) {
 					logger.error({ message: e.message, stack: e.stack });
 				}
